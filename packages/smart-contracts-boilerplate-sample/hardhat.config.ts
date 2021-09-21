c1d9f6bb9bb0caaac7da9009a2bed03706518ba6
Alex Xiong <alex.xiong.tech@gmail.com>
2021-09-21 22:43:44 +0800

    docs: add sol coverage

--- packages/smart-contracts-boilerplate-sample/hardhat.config.ts
+++ packages/smart-contracts-boilerplate-sample/hardhat.config.ts
@@ -1,5 +1,6 @@
 import { task, HardhatUserConfig } from 'hardhat/config';
 import '@nomiclabs/hardhat-waffle';
+import 'solidity-coverage';
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
