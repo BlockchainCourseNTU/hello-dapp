7af6743472d391a406ee73a463a1a2b8446be63f
Alex Xiong <alex.xiong.tech@gmail.com>
2021-09-21 20:59:10 +0800

    docs: add init ts sample hardhat project

---
+++ packages/smart-contracts-boilerplate-sample/hardhat.config.ts
@@ -0,0 +1,30 @@
+import { task, HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.0',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  }
+}
+
+export default config;
