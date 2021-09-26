d4ad66ad02d7c1b8a8c55f9a1fe80aa45cbd49a8
Alex Xiong <alex.xiong.tech@gmail.com>
2021-09-26 14:18:52 +0800

    docs: add deployment to localhost

---
+++ packages/smart-contracts-boilerplate-sample/scripts/test-local-deployed-greeter.ts
@@ -0,0 +1,17 @@
+import { ethers, deployments } from 'hardhat';
+import { assert } from 'chai';
+import { Greeter } from '@typechained';
+
+async function main() {
+  const greeterDeployment = await deployments.get('Greeter');
+
+  const greeter = (await ethers.getContractAt(greeterDeployment.abi, greeterDeployment.address)) as Greeter;
+  assert((await greeter.greet()) === 'Bonjour localhost!');
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
